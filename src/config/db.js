c92f0b2cf24213960ee6df87f10486f4a5f0fc24
elvionweb <elvionweb@gmail.com>
2026-01-21 02:25:48 +0100

    made some change in the server and db files

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,38 @@
+// src/config/db.js
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    
-    console.log("✅ MongoDB connected");
-  } catch (error) {
-    console.error("❌ MongoDB connection failed", error);
-    process.exit(1);
+  if (cached.conn) {
+    return cached.conn; // reuse existing connection
   }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+    }).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
+
+
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+    
+//     console.log("✅ MongoDB connected");
+//   } catch (error) {
+//     console.error("❌ MongoDB connection failed", error);
+//     process.exit(1);
+//   }
+// };
--- src/server.js
+++ src/server.js
@@ -3,7 +3,22 @@ import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
-connectDB();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+connectDB().then(() => {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+}).catch((err) => {
+  console.error("❌ Failed to connect to DB", err);
+});
+
+
+
+// import dotenv from "dotenv";
+// import app from "./app.js";
+// import { connectDB } from "./config/db.js";
+
+// dotenv.config();
+// connectDB();
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
